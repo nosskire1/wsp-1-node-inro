14d187a84934a6de04116280fe82a42e210b6219
nosskire1 <viktor.eriksson3@elev.ga.ntig.se>
2023-01-17 14:29:55 +0100

    .

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+const indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -10,3 +13,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error.njk');
+});
+
